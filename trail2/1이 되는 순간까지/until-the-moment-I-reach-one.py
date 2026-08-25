1f8de83d38f4b5b022514cdaaa9666be41909bb6
seojeongyun <111347014+seojeongyun@users.noreply.github.com>
2026-08-25 23:44:05 +0900

    [Passed] 1이 되는 순간까지 (48ms, 16MB)

--- trail2/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ trail2/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -1,19 +1,34 @@
 N = int(input())
 
 # Please write your code here.
+# Method 1
+# def recursive(n, cnt):
+#     if n == 1:
+#         answer.append(cnt)
+#         return
+
+#     # 짝수면 2로 나눈 몫
+#     if n % 2 == 0:
+#         recursive(n // 2, cnt+1)
+    
+#     # 홀수면 3으로 나눈 몫
+#     else:
+#         recursive(n // 3, cnt+1)
+
+# answer = []
+# recursive(N, 0)
+# print(answer)
+
+
+# Method 2
 def recursive(n, cnt):
     if n == 1:
-        answer.append(cnt)
-        return
+        return cnt
 
-    # 짝수면 2로 나눈 몫
     if n % 2 == 0:
-        recursive(n // 2, cnt+1)
+        return recursive(n // 2, cnt+1)
     
-    # 홀수면 3으로 나눈 몫
-    else:
-        recursive(n // 3, cnt+1)
+    else: 
+        return recursive(n // 3, cnt+1)
 
-answer = []
-recursive(N, 0)
-print(answer[0])
+print(recursive(N, 0))
