0ad9597bacb8e74bd43b4c29595a4e06bb10c0ff
seojeongyun <111347014+seojeongyun@users.noreply.github.com>
2026-08-27 17:17:23 +0900

    [Passed] 국영수 순이지 (52ms, 16MB)

---
+++ trail2/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,20 @@
+n = int(input())
+name = []
+korean = []
+english = []
+math = []
+
+for _ in range(n):
+    student_info = input().split()
+    name.append(student_info[0])
+    korean.append(int(student_info[1]))
+    english.append(int(student_info[2]))
+    math.append(int(student_info[3]))
+
+# Please write your code here.
+
+answer = list(zip(name, korean, english, math))
+answer.sort(key=lambda x:(-x[1], -x[2], -x[3]))
+
+for answ in answer:
+    print(*answ)
