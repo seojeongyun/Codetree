7f5a0de1f1785db86bce3b9473e19f99bb6ced11
seojeongyun <111347014+seojeongyun@users.noreply.github.com>
2026-09-08 14:27:04 +0900

    [Passed] 2진수로 변환하기 (59ms, 16MB)

--- trail2/2진수로 변환하기/convert-to-binary.py
+++ trail2/2진수로 변환하기/convert-to-binary.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 N = int(input().strip())
 
 # [1] 함수 이용
-# print(bin(N)[2:])
+print(bin(N)[2:])
 
 # [2] 나눗셈 이용
-bin_lst = []
-if N == 0:
-    print(0)
-while N > 0:
-    bin_lst.append(N % 2)
-    N = N // 2
+# bin_lst = []
+# if N == 0:
+#     print(0)
+# while N > 0:
+#     bin_lst.append(N % 2)
+#     N = N // 2
 
-for bit in bin_lst[::-1]:
-    print(bit, end='')
+# for bit in bin_lst[::-1]:
+#     print(bit, end='')
     
