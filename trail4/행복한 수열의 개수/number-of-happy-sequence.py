c1a56a5635ac25ddf7ec98deed7e2326f8d20919
seojeongyun <111347014+seojeongyun@users.noreply.github.com>
2026-09-08 10:23:34 +0900

    [Passed] 행복한 수열의 개수 (61ms, 17MB)

--- trail4/행복한 수열의 개수/number-of-happy-sequence.py
+++ trail4/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,49 +1,41 @@
-# 격자 : N x N
-    # 1 이상 100 이하의 정수로만 구성
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-# 행복한 수열: 동일한 원소가 연속하여 M개 이상 나오는 구간이 존재하는 수열
+# Please write your code here.
+'''
+1~100이하 정수 NxN
+행복한 수열 = 동일 원소 연속 M개 나오는 구간 존재
 
-# 2N개의 수열 중 행복한 수열의 개수
-    # 2N: 가로로 N개, 세로로 N개
-
-# 제한 조건
-# N <= 100 이기 때문에, O(N^4) 이내로만 풀면 됨
-
-import sys
-input = sys.stdin.readline
-
-def is_happy_seq(lst):
-    for i in range(N):
-        compare = [lst[i]]
-        for j in range(M):
-            # 범위 밖으로 넘어가면 계산 스킵
-            if i + j >= N:
-                continue
-            if compare[0] != lst[i+j]:
-                break
-            else:
-                compare.append(lst[i+j])
-        if len(compare) == M+1:
+1xN, Nx1로 얻어지는 2N개의 수열 중
+행복한 수열 개수 출력
+'''
+happy = 0
+# 완전 탐색
+def find_happy(arr):
+    cnt = 1
+    # 원소 하나만 연속해도 행복한 수열
+    if m == 1:
+        return 1
+        
+    for i in range(1,n):
+        if arr[i] == arr[i-1]:
+            cnt+=1
+        else:
+            cnt=1
+        
+        if cnt >= m:
             return 1
     return 0
-            
-            
-# [0] 입력
-N, M = map(int, input().strip().split())
-arr = [list(map(int, input().strip().split())) for _ in range(N)]
-answer = 0
 
-# [1] 가로 탐색
-for i in range(N):
-    if is_happy_seq(arr[i]):
-        answer += 1
+# row에서 연속된 수 찾기
+for i in range(n):
+    row = grid[i]
+    happy += find_happy(row)
 
-# [2] 세로 탐색
-for j in range(N):
-    lst = []
-    for i in range(N):
-        lst.append(arr[i][j])
-    if is_happy_seq(lst):
-        answer += 1 
 
-print(answer)
+# col에서 연속된 수 찾기
+for j in range(n):
+    col = [grid[i][j] for i in range(n)]
+    happy += find_happy(col)
+
+print(happy)
